407c3695b33bf7301bc5ea01bc8db52fce548643
Lewandy Dilone Bonifacio <lewandydilone@gmail.com>
2019-10-07 21:07:43 -0400

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@
 
 import './js/env.js';
 import './css/signin.css';
-
-import 'jquery';
-import 'popper.js/dist/popper';
-
-import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
-// import './js/login.js';
 
 
