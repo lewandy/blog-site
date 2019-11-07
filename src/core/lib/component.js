61c141e9b59d5d03610f5672f6de368f46cda21e
Lewandy Dilone Bonifacio <lewandydilone@gmail.com>
2019-11-07 15:09:53 -0400

    refactoring core register components

--- src/core/lib/component.js
+++ src/core/lib/component.js
@@ -5,14 +5,7 @@
  */
 export function register(components) {
    for (let component of components) {
-      if (!window.app_components)
-         window.app_components = [component.name]
-      else if (window.app_components.includes(component.name))
-         console.error(`Component called:${component.name} is already registered.`)
-      else {
-         window.app_components.push(component.name);
-         customElements.define(component.name, component);
-      }
+      customElements.define(component.name, component);
    }
 }
 
