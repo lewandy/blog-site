6f021f4a223d6647ccca8d5eb37ffa419ecacd6e
Lewandy Dilone Bonifacio <lewandydilone@gmail.com>
2019-11-07 15:10:23 -0400

    add some components to the app

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
 import App from "../core/app";
 import Router from "../core/router";
 import { TheAsideComponent, TheFooterComponent, TheHeaderComponent } from "./components/layout"
+import PostsComponent from "./components/PostsComponent";
 
 export default class BlogApp extends App {
    router = null;
 
    constructor(configuration) {
-      super([TheFooterComponent, TheHeaderComponent, TheAsideComponent])
+      super([
+         TheFooterComponent,
+         TheHeaderComponent, 
+         TheAsideComponent,
+         PostsComponent
+      ]);
 
       this.router = new Router({ routes: configuration.routes });
       window.app = this;
