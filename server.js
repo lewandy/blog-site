3cc3860f7d92c6256606e299c4370e823ef9e3c3
Lewandy Dilone <lewandydilone1@live.com>
2019-11-18 01:31:42 -0400

    add server with node js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 var app = express();
 
-app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+module.exports = app;
